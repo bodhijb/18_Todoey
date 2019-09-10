1d9c1f7d6f2a1d7c6cb26e5777cece050005bcd3
Barbara Joseph <gehertt@gmail.com>
2019-09-10 19:47:15 +0100

    "Todo list complete with datasource and delegate methods"

---
+++ 18_Todoey/TodoListViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  18_Todoey
+//
+//  Created by Barbara Joseph on 10/09/2019.
+//  Copyright © 2019 Gehertt. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let itemArray: [String] = ["Find Mike", "Buy Eggs", "Destroy Demogorgon", ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+    
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item
+        
+        return cell
+    }
+    
+    
+    // MARK - Tableview Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+    
+    
+    
+    
+    
+
+
+}
+
